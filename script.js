727f73d609a4a19f892727a8a9483c5d07c25f02
coroansrj <kauanzin.souza12@gmail.com>
2026-02-26 02:15:59 -0300

    Cantinho do Cookie

---
+++ script.js
@@ -0,0 +1,162 @@
+const menu = document.getElementById("menu")
+const cartBtn = document.getElementById("cart-btn")
+const cartModal = document.getElementById("cart-modal")
+const cartItemsContainer = document.getElementById("cart-items")
+const cartTotal = document.getElementById("cart-total")
+const checkoutBtn = document.getElementById("checkout-btn")
+const closeModalBtn = document.getElementById("close-modal-btn")
+const cartCounter = document.getElementById("cart-count")
+const addressInput = document.getElementById("address")
+const addressWarn = document.getElementById("address-warn")
+
+
+let cart = [];
+
+
+
+// Abrir Modal do Carrinho
+cartBtn.addEventListener("click", function() {
+    updateCartModal();
+    cartModal.style.display = "flex"
+})
+
+// Fechar o Modal quando Clicar Fora
+cartModal.addEventListener("click", function(e) {
+    if(e.target === cartModal) {
+        cartModal.style.display = "none"
+    }
+})
+
+closeModalBtn.addEventListener("click", function() {
+    cartModal.style.display = "none"
+})
+
+menu.addEventListener("click", function(e) {
+    let parentButton = e.target.closest(".add-to-cart-btn")
+    if(parentButton) {
+        const name = parentButton.getAttribute("data-name")
+        const price = parseFloat(parentButton.getAttribute("data-price"))
+
+        // Add ao Carrinho
+        addToCart(name, price)
+    }
+
+})
+
+// Funçao para Add ao carrinho
+function addToCart(name, price) {
+
+    const existingItem = cart.find(item => item.name === name)
+
+    if(existingItem){
+        // Se o item ja existe apenas acrescenta +1
+        existingItem.quantity += 1;
+    } else {
+        cart.push({
+        name,
+        price,
+        quantity: 1,
+    })
+    }
+
+    updateCartModal()
+
+}
+
+// Atualiza o Carrinho
+function updateCartModal() {
+    cartItemsContainer.innerHTML = "";
+    let total = 0;
+
+    cart.forEach(item => {
+        const cartItemElement = document.createElement("div");
+        cartItemElement.classList.add("flex", "justify-between", "mb-4", "flex-col")
+
+        cartItemElement.innerHTML = `
+            <div class="flex items-center justify-between">
+                <div>
+                    <p class="font-medium">${item.name}</p>
+                    <p>Quantidade: ${item.quantity}</p>
+                    <p class="font-medium mt-2">R$ ${item.price.toFixed(2)}</p>
+                </div>
+                <button class="remove-from-cart-btn" data-name="${item.name}">
+                    Remover
+                </button>
+            </div>
+        `
+
+        total += item.price * item.quantity;
+
+        cartItemsContainer.appendChild(cartItemElement)
+
+    })
+
+
+    cartTotal.textContent = total.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    });
+
+    cartCounter.innerHTML = cart.length;
+
+}
+
+// Funcao para remover item carrinho
+cartItemsContainer.addEventListener("click", function(e) {
+    if(e.target.classList.contains("remove-from-cart-btn")){
+        const name = e.target.getAttribute("data-name")
+
+        removeItemCart(name)
+    }
+})
+
+function removeItemCart(name){
+    const index = cart.findIndex(item => item.name === name);
+
+    if(index !== -1){
+        const item = cart[index];
+
+        if(item.quantity > 1){
+            item.quantity -=1;
+            updateCartModal()
+            return;
+        }
+
+        cart.splice(index, 1);
+        updateCartModal();
+    }
+}
+
+addressInput.addEventListener("input", function(e) {
+    let inputValue = e.target.value;
+
+    if(inputValue !== "") {
+        addressInput.classList.remove("border-red-500")
+        addressWarn.classList.add("hidden")
+    }
+})
+
+//Finalizar Pedido
+checkoutBtn.addEventListener("click", function() {
+    if(cart.length === 0) return;
+    if(addressInput.value === "") {
+        addressWarn.classList.remove("hidden")
+        addressInput.classList.add("border-red-500")
+        return;
+    }
+
+    const cartItems = cart.map((item) => {
+        return (
+            `${item.name} Quantidade: (${item.quantity}) Preço: ${item.price} | `
+        )
+    }).join("")
+
+    const message = encodeURIComponent(cartItems)
+    const phone = "21975398758"
+
+
+    window.open(`https://wa.me/${phone}?text=${message} Forma de Pagamento: ${addressInput.value}`, "_blank")
+
+    cart = [];
+    updateCartModal();
+})
